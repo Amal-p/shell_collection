6dfbf8e7d50d444b1f5ab52d11bc37bae830fbc9
amal.p <amal.p@infospica.com>
2022-12-01 17:05:10 +0530

    first shell added

---
+++ xampp.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+SERVICE=mysql;
+PASSWORD=Bluerose67;
+
+if ps ax | grep -v grep | grep $SERVICE > /dev/null
+then
+    echo "$SERVICE service running"
+    echo $PASSWORD | sudo -S service mysql stop
+    sleep 2
+    echo "$SERVICE service stoped"
+fi
+    
+SERVICE=apache2;
+
+if ps ax | grep -v grep | grep $SERVICE > /dev/null
+then
+    echo "$SERVICE service running"
+    echo $PASSWORD | sudo -S systemctl stop apache2
+    sleep 2
+    echo "$SERVICE service stoped"
+fi
+    
+SERVICE=nginx;
+
+if ps ax | grep -v grep | grep $SERVICE > /dev/null
+then
+    echo "$SERVICE service running"
+   echo $PASSWORD | sudo -S systemctl stop nginx
+    sleep 2
+    echo "$SERVICE service stoped"
+fi
+    
+sleep 2
+
+echo $PASSWORD | sudo -S /opt/lampp/lampp start
+
+sleep 2
+sudo /opt/lampp/manager-linux-x64.run
