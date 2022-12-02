f551584eda1fdf47a299605b3d252767b32eabb0
Amal P Vatayam <51821900+Amal-p@users.noreply.github.com>
2022-12-02 15:04:44 +0530

    Create python.sh

---
+++ python.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt-get install python3
+sudo apt-get install python3-pip
+python3 -m pip install selenium
