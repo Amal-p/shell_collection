2432fb8c7e192e274dc660cdd5f4752f3cf0084b
Amal P Vatayam <51821900+Amal-p@users.noreply.github.com>
2022-12-02 15:08:05 +0530

    Update python.sh

--- python.sh
+++ python.sh
@@ -2,6 +2,7 @@
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo rm -rf google-chrome-stable_current_amd64.deb
 sudo apt-get install python3
 sudo apt-get install python3-pip
 python3 -m pip install selenium
